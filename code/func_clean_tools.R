7d84607abcc53a171a529f9bf0a2f35bba7063ea
Yingjie Li <yingjieli.edu@gmail.com>
2023-12-07 00:17:10 -0800

    update `func_clean_tools()`

--- code/func_clean_tools.R
+++ code/func_clean_tools.R
@@ -1,53 +1,57 @@
 
 
-func_clean_tools <- function(data) {
+func_clean_tools <- function(data, column_name) {
   d <- data %>%
     dplyr::mutate(
-      Tool = gsub("Other: ", "", Tool),
-      Tool = gsub("scales|Scales", "scale", Tool, ignore.case = T),
-      Tool = gsub("\\s*\\([^\\)]+\\)", "", Tool), # remove text within parenthesis 
-      Tool = gsub(
+      !!column_name := gsub("Other: ", "", !!sym(column_name)),
+      !!column_name := gsub("scales|Scales", "scale", !!sym(column_name), ignore.case = T),
+      ## remove text within parenthesis 
+      !!column_name := gsub("\\s*\\([^\\)]+\\)", "", !!sym(column_name)), 
+      !!column_name := gsub(
         paste("Anti\\-depressant prescribing", "Antidepressant prescriptions", 
               "Antidepressant prescription rates", "Antidepressant use", sep = "|"),
-        "Antidepressant prescription", Tool, ignore.case = T),
-      Tool = gsub("CES\\-D\\-10|CESD\\-10", "CES\\-D10", Tool, ignore.case = T),
-      Tool = gsub("CES\\-D\\-20|CESD\\-20", "CES\\-D20", Tool, ignore.case = T),
-      Tool = gsub(
-        paste("Diagnosed", "Diagnoses", "Diagnosis by professionals", 
+        "Antidepressant prescription", !!sym(column_name), ignore.case = T),
+      !!column_name := gsub("CES\\-D\\-10|CESD\\-10", "CES\\-D10", !!sym(column_name), ignore.case = T),
+      !!column_name := gsub("CES\\-D\\-20|CESD\\-20", "CES\\-D20", !!sym(column_name), ignore.case = T),
+      !!column_name := gsub(
+        paste("Diagnosed", "Diagnoses", 
+              "Diagnosis by professionals", 
               "Diagnosis of mental health disorders", sep = "|"), 
-        "Diagnosis", Tool, ignore.case = T),
-      Tool = gsub("Digit Span Backward", "DSB", Tool, ignore.case = F), ## 
-      Tool = case_when(Tool %in% c('Digit', 'digit', 'Digit-span Task') ~ "Digit Span",
-                       Tool == 'Digit test' ~ "Digit Span",
-                       T ~ Tool),
-      Tool = gsub("Electroencephalography", "EEG", Tool, ignore.case = F), ## # 
-      Tool = gsub("EQ-5D", "EQ5D", Tool, ignore.case = F), ## # 
-      Tool = gsub("K\\-10", "K10", Tool, ignore.case = F),
-      Tool = gsub("Life Satisfaction approach", 'Life satisfaction approach', Tool, ignore.case = T),
-      Tool = gsub(
-        paste("Official/National mental health", "National survey", "Office for National Statistics", 
+        "Diagnosis", !!sym(column_name), ignore.case = T),
+      !!column_name := gsub("Digit Span Backward", "DSB", !!sym(column_name), ignore.case = F), ## 
+      !!column_name := case_when(!!sym(column_name) %in% c('Digit', 'digit', 'Digit-span Task') ~ "Digit Span",
+                       !!sym(column_name) == 'Digit test' ~ "Digit Span",
+                       T ~ !!sym(column_name)),
+      !!column_name := gsub("Electroencephalography", "EEG", !!sym(column_name), ignore.case = F), ## # 
+      !!column_name := gsub("EQ-5D", "EQ5D", !!sym(column_name), ignore.case = F), ## # 
+      !!column_name := gsub("K\\-10", "K10", !!sym(column_name), ignore.case = F),
+      !!column_name := gsub("Life Satisfaction approach", 'Life satisfaction', !!sym(column_name), ignore.case = T),
+      !!column_name := gsub(
+        paste("Official\\/National mental health", 
+              "National survey", 
+              "Office for National Statistics", 
               "UK's Office of National Statistics",
               sep = "|"),
-        'Official MH survey', Tool, ignore.case = T),#
-      Tool = gsub("survey survey", 'survey', Tool, ignore.case = T),# 
-      Tool = gsub("Stroop Test", 'Stroop test', Tool, ignore.case = F),# 
-      Tool = gsub("PNAS", 'PANAS', Tool, ignore.case = T),# 
-      Tool = gsub("Profile of mood state questionnaire", 'POMS', Tool, ignore.case = T),# 
-      Tool = gsub("Psychiatric disorders", 'Mental disorder', Tool, ignore.case = T),# 
-      Tool = gsub("Psychiatric disorder", 'Mental disorder', Tool, ignore.case = T),
-      Tool = gsub("PHQ-2|PHQ-4|PHQ-8|PHQ-9", 'PHQ', Tool, ignore.case = T),
-      Tool = gsub("RAND-36", 'RAND36', Tool, ignore.case = T), # # 
-      Tool = gsub("Restoration Outcome Scale|ROS\\-S", 'ROS', Tool, ignore.case = T), # 
-      Tool = gsub("RSE or RSES|RSES", 'RSE', Tool, ignore.case = F), # 
-      Tool = gsub("SCL90", 'SCL-90', Tool, ignore.case = F), # 
-      Tool = gsub("SWEMWBS|WEMWBS 7-item", 'WEMWBS', Tool, ignore.case = F), # # 
-      Tool = gsub(",+", ",", Tool), ## Removing repeated punctuation character from a string
-      Tool = gsub(";+", ";", Tool), ## Removing repeated punctuation character from a string
-      Tool = trimws(Tool),
-      Tool = str_squish(Tool)
+        'Official MH survey', !!sym(column_name), ignore.case = T),#
+      !!column_name := gsub("survey survey", 'survey', !!sym(column_name), ignore.case = T),# 
+      !!column_name := gsub("Stroop Test", 'Stroop test', !!sym(column_name), ignore.case = F),# 
+      !!column_name := gsub("PNAS", 'PANAS', !!sym(column_name), ignore.case = T),# 
+      !!column_name := gsub("Profile of mood state questionnaire", 'POMS', !!sym(column_name), ignore.case = T),# 
+      !!column_name := gsub("Psychiatric disorders", 'Mental disorder', !!sym(column_name), ignore.case = T),# 
+      !!column_name := gsub("Psychiatric disorder", 'Mental disorder', !!sym(column_name), ignore.case = T),
+      !!column_name := gsub("PHQ-2|PHQ-4|PHQ-8|PHQ-9", 'PHQ', !!sym(column_name), ignore.case = T),
+      !!column_name := gsub("RAND-36", 'RAND36', !!sym(column_name), ignore.case = T), # # 
+      !!column_name := gsub("Restoration Outcome Scale|ROS\\-S", 'ROS', !!sym(column_name), ignore.case = T), # 
+      !!column_name := gsub("RSE or RSES|RSES", 'RSE', !!sym(column_name), ignore.case = F), # 
+      !!column_name := gsub("SCL90", 'SCL-90', !!sym(column_name), ignore.case = F), # 
+      !!column_name := gsub("SWEMWBS|WEMWBS 7-item", 'WEMWBS', !!sym(column_name), ignore.case = F), # # 
+      !!column_name := gsub(",+", ",", !!sym(column_name)), ## Removing repeated punctuation character from a string
+      !!column_name := gsub(";+", ";", !!sym(column_name)), ## Removing repeated punctuation character from a string
+      !!column_name := trimws(!!sym(column_name)),
+      !!column_name := str_squish(!!sym(column_name))
       
-    ) %>% ## remove any strings in a ()
-    dplyr::filter(Tool != 'NA') %>%
+    ) %>% 
+    # dplyr::filter(!!sym(column_name) != 'NA') %>%
     as.data.frame()
   
   return(d)
