46529216f66bf20861372a58af45bdf04fe4528e
Yingjie Li <yingjieli.edu@gmail.com>
2023-12-21 17:08:08 -0800

    add new functions

---
+++ code/func_make_gradient.R
@@ -0,0 +1,41 @@
+
+library(ggplot2) 
+library(grid)
+library(RColorBrewer)
+
+make_gradient <- function(deg = 45, n = 100, cols = blues9, col_rev = T, alpha = 0.5) {
+  cols <- colorRampPalette(cols)(n + 1)
+  cols <- scales::alpha(cols, 0.5)
+  
+  if (col_rev == T) {
+    cols <- rev(cols)
+  }
+  rad <- deg / (180 / pi)
+  mat <- matrix(
+    data = rep(seq(0, 1, length.out = n) * cos(rad), n),
+    byrow = TRUE,
+    ncol = n
+  ) +
+    matrix(
+      data = rep(seq(0, 1, length.out = n) * sin(rad), n),
+      byrow = FALSE,
+      ncol = n
+    )
+  mat <- mat - min(mat)
+  mat <- mat / max(mat)
+  mat <- 1 + mat * n
+  mat <- matrix(data = cols[round(mat)], ncol = n)
+  grid::rasterGrob(
+    image = mat, 
+    width = unit(1, "npc"),
+    height = unit(1, "npc"), 
+    interpolate = TRUE
+  )
+}
+
+
+## test 
+# g <- make_gradient(deg = 180, n = 500, cols = brewer.pal(9, "RdBu"))
+# 
+# p1 + 
+#   annotation_custom(grob = g, xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = Inf) 
---
+++ code/func_plot_ma.R
@@ -0,0 +1,53 @@
+# Install and load necessary packages
+# install.packages(c("ggplot2", "metafor"))
+library(ggplot2)
+source('./code/func_ggsave.R')
+
+# Create a forest plot-style ggplot
+
+plot_ma <- function(data) {
+  
+  
+  x_limit_max <- max(abs(data$es.lower), abs(data$es.upper)) * 1.1
+  
+  g <- make_gradient(deg = 180, n = 500, cols = brewer.pal(9, "RdBu"))
+  
+  
+  p <- data %>%
+    dplyr::mutate(n_lab = paste0('n = ', n_study)) %>%
+    ggplot(., 
+           aes(x = es.mean, 
+               y = reorder(ind_sub, es.mean))) +
+    
+    annotation_custom(grob = g, xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = Inf) +
+    # xlim(-x_limit_max, x_limit_max) +
+    scale_x_continuous(n.breaks = 10, limits = c(-x_limit_max, x_limit_max)) + 
+  
+    geom_point(aes(x = es.mean), size = 3) +
+    geom_errorbarh(aes(xmin = es.lower, xmax = es.upper), height = 0.2) +
+    geom_text(aes(x = es.mean, label = n_lab), vjust = -1, size = 3) +
+    geom_text(aes(x = es.mean, label = p.star), vjust = 1.5, size = 5) +
+    geom_vline(xintercept = 0, linetype = "dashed", color = "grey") +
+    labs(title = "",
+         x = "Effect Size",
+         y = "") +
+    theme_minimal() +
+    theme(panel.grid.major.y = element_blank(),
+          panel.grid.minor.y = element_blank())
+  
+  
+  p
+  ## save plot
+  fname <- paste0(dir.fig, 'plot_es_', unique(data$tool) ,'.png'); fname
+  func_ggsave(fname, w = 6, h = 4, save_png = T)
+  
+  return(p)
+  
+}
+
+
+## test
+plot_ma(data = ma_result)
+p1 <- plot_ma(data = ma_result)
+
+
