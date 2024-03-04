388ca92c8f4f4a3bb7a26f44c37c5d38b43ec62d
Yingjie Li <yingjieli.edu@gmail.com>
2024-03-03 23:31:54 -0800

    update

--- code/func_clean_indicatorsPro.R
+++ code/func_clean_indicatorsPro.R
@@ -7,19 +7,22 @@ func_clean_indicatorsPro <-  function(data, column_name) {
       !!column_name := gsub("Other\\: |in general\\*", "", !!sym(column_name)),
       !!column_name := gsub("\\(include schizophrenia\\)", "", !!sym(column_name)),
       !!column_name := gsub("\\s*\\([^\\)]+\\)", "", !!sym(column_name)), # remove text within parenthesis 
+      
       !!column_name := gsub("\\/Restorativeness", "", !!sym(column_name)),
-      !!column_name := gsub("Restoration experience|Restorative experience|Restorativeness", 
+      !!column_name := gsub("Restoration experience|Restorative experience|Restorativeness|Psychological Restorative Effects", 
                             "Restorative effects", !!sym(column_name), ignore.case = T), 
-      !!column_name := gsub("Attention capacity|Sustained attention", "Attention", !!sym(column_name), ignore.case = T), ##  
+      !!column_name := gsub("Restoration", "Restorative effects", !!sym(column_name)),
+      !!column_name := gsub("Attention capacity|Sustained attention|Attention Functioning", "Attention", !!sym(column_name), ignore.case = T), ##  
       !!column_name := gsub("Behavioural", "Behavioral", !!sym(column_name), ignore.case = T), ##  
       !!column_name := gsub("Behavioral problems", "Behavioral disorder", !!sym(column_name), ignore.case = T), ## 
       !!column_name := gsub("Burnout symptom", "Burnout", !!sym(column_name), ignore.case = T),
       !!column_name := gsub("Burnout fatigue|burnout fatigue", "Burnout", !!sym(column_name), ignore.case = T),
       !!column_name := gsub("Cognitions and Conduct", "Cognitive functioning", !!sym(column_name), ignore.case = T), # 
-      !!column_name := gsub("Quality of life|Quality of Life", "Quality of life;", !!sym(column_name)),# 
+      !!column_name := gsub("Quality of life|Quality of Life|Recovering Quality Of Life", "Quality of life;", !!sym(column_name)),# 
       !!column_name := gsub("Psychological distress|psychological distress", "Mental distress", !!sym(column_name)),# # 
       !!column_name := gsub("psychiatric morbidity|psychiatric disorder|mental illness", "Mental disorder", !!sym(column_name), ignore.case = T),
       !!column_name := gsub("Symptoms of psychosis", "Psychosis", !!sym(column_name), ignore.case = T), #### 
+      !!column_name := gsub("Subjective Vitality", "Vitality", !!sym(column_name), ignore.case = T),
       
       !!column_name := gsub(",", ";", !!sym(column_name)),
       !!column_name := trimws(!!sym(column_name))
@@ -33,7 +36,7 @@ func_clean_indicatorsPro <-  function(data, column_name) {
 
 
 
-func_clean_indicator_sub <-  function(data, column_name) {
+func_clean_indicator_sub <-  function(data, column_name, upper_case = T) {
   d <- data %>%
     dplyr::mutate(
       
@@ -44,8 +47,8 @@ func_clean_indicator_sub <-  function(data, column_name) {
       !!column_name := gsub("A\\-H|anger\\-hostility|anger and hostility", "Anger", !!sym(column_name), ignore.case = T),
       !!column_name := gsub("hostility", "Anger", !!sym(column_name), ignore.case = T),
       !!column_name := gsub("Confused|confusion\\-bewilderment", "Confusion", !!sym(column_name), ignore.case = T),
-      !!column_name := gsub("depression-dejection|confusion\\-bewilderment|depression and dejection", "Confusion", !!sym(column_name), ignore.case = T),
-      !!column_name := gsub("fatigue\\-inertia", "Fatigue", !!sym(column_name), ignore.case = T),
+      !!column_name := gsub("depression-dejection|depression and dejection", "Depression", !!sym(column_name), ignore.case = T),
+      !!column_name := gsub("fatigue\\-inertia|Mental Fatigue", "Fatigue", !!sym(column_name), ignore.case = T),
       !!column_name := gsub("tension\\-anxiety|tension and anxiety|Tension\\/Anxiety|T\\-A", "Tension Anxiety", !!sym(column_name), ignore.case = T),
       !!column_name := gsub("Tension Anxiety|Tension", "Anxiety", !!sym(column_name), ignore.case = T),
       !!column_name := gsub("total mood disturbance|Total Mood of Disturbance", "TMD", !!sym(column_name), ignore.case = T),
@@ -59,13 +62,23 @@ func_clean_indicator_sub <-  function(data, column_name) {
       !!column_name := gsub("Positive Affect", "Positive Affect", !!sym(column_name), ignore.case = T),
       !!column_name := gsub("Negative Affect", "Negative Affect", !!sym(column_name), ignore.case = T),
       
-      !!column_name := ifelse(
-        !!sym(column_name) == toupper(!!sym(column_name)), !!sym(column_name),
-        stringr::str_to_title(!!sym(column_name))
-        ),
       !!column_name := trimws(!!sym(column_name))
     ) %>%
     as.data.frame()
+  
+  
+  if(upper_case == T) {
+    d <- data %>%
+      dplyr::mutate(
+        !!column_name := ifelse(
+          !!sym(column_name) == toupper(!!sym(column_name)), !!sym(column_name),
+          stringr::str_to_title(!!sym(column_name))
+        )
+      ) 
+  } else {
+    d <- d
+  }
+      
   
   return(d)
 }
