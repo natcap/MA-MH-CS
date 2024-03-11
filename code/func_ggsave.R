90d4c353728b7532f9f43e376092cbf4f1b80469
Yingjie Li <yingjieli.edu@gmail.com>
2024-03-10 23:00:11 -0700

    minor edits

--- code/func_ggsave.R
+++ code/func_ggsave.R
@@ -1,7 +1,7 @@
 
-func_ggsave <- function(fname, w = 7, h = 4, save_png = save_plot) {
+func_ggsave <- function(fname, w = 7, h = 4, dpi = 300, save_png = save_plot) {
   if (save_png == T) {
-    ggsave(filename = fname, plot = last_plot(), width = w, height = h, units = "in", dpi = 300)
+    ggsave(filename = fname, plot = last_plot(), width = w, height = h, units = "in", dpi = dpi)
   } else {
     print('The plot will not be saved.')
   }
