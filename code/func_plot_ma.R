7ac209d888b6fb0c7a276ed78123c57c23e290fc
Yingjie Li <yingjieli.edu@gmail.com>
2024-04-06 11:46:36 -0700

    add more flexibility to the function

--- code/func_plot_ma.R
+++ code/func_plot_ma.R
@@ -14,8 +14,16 @@ plot_effect_size_overall <- function(
     subgroup = NULL, 
     dodge_value = 0.9,
     facet_bygroup = F,
+    facet_scales = 'free',
     add_gradient_bg = T,
+    text_size = 12,
     show_legend = F) {
+  
+  theme_set(theme_minimal() + 
+              theme(text = element_text(size = text_size), 
+                    axis.text = element_text(size = text_size),
+                    strip.text = element_text(size = text_size)
+                    )) # Applies a base size
   
   # x_limit_max <- max(abs(data$es.lower), abs(data$es.upper), na.rm = T) * 1.1
   
@@ -36,8 +44,9 @@ plot_effect_size_overall <- function(
       p <- data %>%
         ggplot(., 
                aes(x = es.mean, 
-                   # y = reorder(ind_sub, desc(abs(es.mean))), # the largest effect on the top 
-                   y = reorder(ind_sub, desc(es.mean)), # the largest effect on the top 
+                   y = ind_sub, 
+                   # y = reorder(ind_sub, desc(abs(es.mean))), # the largest effect on the top
+                   # y = reorder(ind_sub, desc(es.mean)),      # the largest effect on the top 
                    group = .data[[color_var]],
                    color = .data[[color_var]]
                    ))
@@ -45,7 +54,8 @@ plot_effect_size_overall <- function(
       p <- data %>%
         ggplot(., 
                aes(x = es.mean, 
-                   y = reorder(ind_sub, desc(es.mean)), # the largest effect on the top 
+                   y = ind_sub, 
+                   # y = reorder(ind_sub, desc(abs(es.mean))),
                ))
     }
     
@@ -82,14 +92,49 @@ plot_effect_size_overall <- function(
     ## 
     # scale_x_continuous(limits = c(-x_limit_max, x_limit_max)) + 
   
-    geom_point(aes(x = es.mean), size = 2, position = position_dodge(dodge_value), show.legend = show_legend) +
-    geom_errorbarh(aes(xmin = es.lower, xmax = es.upper), height = 0.2, position=position_dodge(width = dodge_value), show.legend = F) +
+    geom_point(aes(x = es.mean), size = 2, 
+               position = position_dodge(dodge_value), show.legend = show_legend) +
+    geom_errorbarh(aes(xmin = es.lower, xmax = es.upper), 
+                   height = 0.15, position=position_dodge(width = dodge_value), show.legend = F) +
     # geom_text(aes(x = es.mean, label = paste0(round(es.mean, digits = 2), ' ', p.star)), 
     #           vjust = 1.7, size = 2.5, position=position_dodge(dodge_value), show.legend = F) +
-    geom_text(aes(x = es.mean, label = round(es.mean, digits = 2)), vjust = 1.7, size = 2.5, position=position_dodge(dodge_value), show.legend = F) +
-    geom_text(aes(x = es.mean, label = p.star), vjust = 0, size = 4, position=position_dodge(dodge_value), show.legend = F) +
+    
+    ## p value label
+    geom_text(aes(x = es.mean, label = p.star), 
+              vjust = 0.2, size = text_size/4, position=position_dodge(dodge_value), show.legend = F) +
     labs(title = "", x = xlab_name, y = "") +
-    guides(color = guide_legend(reverse=TRUE)) 
+    guides(color = guide_legend(reverse=F)) 
+  
+  
+  
+  ## for non-subgroup analysis
+  if (is.null(subgroup)) { 
+    p <- p +
+      ## effect size label
+      geom_text(aes(x = es.mean, label = round(es.mean, digits = 2)), 
+                vjust = 1.7, size = text_size/4, 
+                position=position_dodge(dodge_value), show.legend = F) +
+      geom_text(aes(x = es.upper, label = n_lab), 
+                vjust = 0,  hjust = -0.2, 
+                size = text_size/4, color='gray40', fontface = "italic",
+                position=position_dodge(dodge_value), show.legend = F) +
+      geom_text(aes(x = es.upper, label = I2_lab), 
+                vjust = 1, hjust = -0.15, 
+                size = text_size/4, color='gray40', fontface = "italic",
+                position=position_dodge(dodge_value), show.legend = F, parse = T)
+    ## remove sample size for subgroups
+  } else {
+    p <- p +
+      ## effect size label
+      geom_text(aes(#x = es.mean/abs(es.mean)*(-2), 
+                    # x = es.mean*1.1,
+                    x = es.mean,
+                    vjust = 1.7, 
+                    label = round(es.mean, digits = 1)), 
+                size = text_size/5, 
+                position=position_dodge(dodge_value), show.legend = F) 
+  }
+  
   
   if (!is.null(color_var)) {
     p <- p + scale_color_brewer(palette = "Dark2", direction = -1) 
@@ -97,21 +142,8 @@ plot_effect_size_overall <- function(
     p <- p
   }
   
-  ## for non-subgroup analysis
-  if (is.null(subgroup)) { 
-    p <- p +
-      geom_text(aes(x = es.upper, label = n_lab), vjust = 0.5-0.1,  hjust = -0.2, size = 2.5, color='gray40', fontface = "italic",
-                position=position_dodge(dodge_value), show.legend = F) +
-      geom_text(aes(x = es.upper, label = I2_lab), vjust = 1.7, hjust = -0.1, size = 2.5, color='gray40', fontface = "italic",
-                position=position_dodge(dodge_value), show.legend = F, parse = T)
-    ## remove sample size for subgroups
-  } else {
-    p <- p
-  }
-  
-  
   p <- p +
-    theme_minimal() +
+    # theme_minimal() +
     theme(panel.grid.major.y = element_blank(),
           panel.grid.minor.x = element_blank(),
           panel.grid.minor.y = element_blank())
@@ -119,7 +151,7 @@ plot_effect_size_overall <- function(
   ## when the number of subgroup is too large, it is better to facet the plot
   if (facet_bygroup == T) {
     p <- p +
-      facet_wrap(~ind_sub, scales = 'free') +
+      facet_wrap(~ind_sub, scales = facet_scales) +
       theme_bw() +
       theme(axis.title.y = element_blank(),
             axis.text.y = element_blank(),
