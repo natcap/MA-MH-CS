2fa8b22bb64fb91336274d4369c98876923e4451
Yingjie Li <yingjieli.edu@gmail.com>
2024-08-15 17:19:08 -0700

    refine function for cleaning effect size: `d` -> `d_cohens`, and formating `coefficients`

--- code/func_clean_effectsize.R
+++ code/func_clean_effectsize.R
@@ -17,27 +17,31 @@ func_clean_effectsize <- function(
       !!column_name := gsub("\\s*\\([^\\)]+\\)", "", !!sym(column_name)), # remove text within parenthesis 
       
       ## text cleaning 
-      # !!column_name := gsub("=.*", "", !!sym(column_name)), ## remove everything after "="
-      !!column_name := gsub(" = correlation using linear regression", "", !!sym(column_name)),
+      !!column_name := gsub(" =", "", !!sym(column_name)), ## remove " = "
+      !!column_name := str_squish(!!sym(column_name)),
+      !!column_name := gsub("correlation using linear regression", "", !!sym(column_name)),
       
-      !!column_name := gsub(" = odds ratio = \\(group1 odds of outcome\\)", "", !!sym(column_name)),
+      !!column_name := gsub("odds ratio\\(group1 odds of outcome\\)", "", !!sym(column_name)),
       !!column_name := gsub("\\/\\(group2 odds of outcome\\)", "", !!sym(column_name)),
-      !!column_name := gsub(" = odds ratio =\\/", "", !!sym(column_name)),
+      !!column_name := gsub("odds ratio\\/", "", !!sym(column_name)),
       
-      !!column_name := gsub(" = Pearson's r correlation \\(range\\: -1 to 1\\)", "", !!sym(column_name)),
-      !!column_name := gsub(" = Pearson's r correlation", "", !!sym(column_name)),
+      !!column_name := gsub("Pearson's r correlation \\(range\\: -1 to 1\\)", "", !!sym(column_name)),
+      !!column_name := gsub("Pearson's r correlation", "", !!sym(column_name)),
       
       
-      !!column_name := gsub(" = Cohen\\’s d = \\(mean1 - mean2\\)\\/sd", "", !!sym(column_name)),
-      !!column_name := gsub(" = Cohen’s d =\\/sd", "", !!sym(column_name)),
-      
+      !!column_name := gsub(" Cohen\\’s d\\(mean1 - mean2\\)\\/sd", "_cohens", !!sym(column_name)),
+      !!column_name := gsub(" Cohen’s d\\/sd", "_cohens", !!sym(column_name)),
+
       !!column_name := gsub("Measure values before and after intervention", "Mean_pre_post", !!sym(column_name), ignore.case = T),
+      !!column_name := gsub("Measure values between different interventions", "Raw values", !!sym(column_name), ignore.case = T),
       !!column_name := gsub("raw values|raw value|raw scores|raw score", "Raw values", !!sym(column_name), ignore.case = T),
       !!column_name := gsub("Incidence Rate Ratios|Incidence Rate Ratio", "IRR", !!sym(column_name), ignore.case = T),
       
       
-      !!column_name := gsub(" = correlation using logistic regression", "", !!sym(column_name)),
-      !!column_name := gsub("corr_linear|corr_logi|regression coefficient|Regression coefficient", "coefficient", !!sym(column_name)),
+      !!column_name := gsub("correlation using logistic regression", "", !!sym(column_name)),
+      !!column_name := gsub("corr_linear|corr_logi|regression coefficient|Regression coefficient|coefficients", "coefficient", !!sym(column_name), ignore.case = T),
+      !!column_name := gsub("Unstandardized coefficient|unstandardized_coef|unstandardized_coef", "coef_unstandardized", !!sym(column_name), ignore.case = T),
+      !!column_name := gsub("unstandardized coef", "coef_unstandardized", !!sym(column_name), ignore.case = T),
       
 
       ## final formatting 
